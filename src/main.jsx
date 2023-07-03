0bfb751ecd9c8b893f17c66ab003cedf900037d3
Dupakarovsky <dupakarovsky@outlook.com>
2023-07-03 18:10:11 -0300

    blank slate

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/App/App.jsx";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
